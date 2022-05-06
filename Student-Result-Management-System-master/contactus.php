3e2a2a84dd13998d53e118dba4680203c5cdf174
Prajjwa-art <83761495+Prajjwa-art@users.noreply.github.com>
2022-05-06 11:32:55 +0530

    Add files via upload

---
+++ Student-Result-Management-System-master/contactus.php
@@ -0,0 +1,23 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact Us</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>location - Mohali</li>
+         	<li>At - CU</li>
+         	<li>Dist - Mohali,Punjab-India</li>
+            <li>Pin - 140301</li>
+         	<li>contact no - 9991170616
+         	</li>
+         </ul>
+         <center><h2>Thank you</h2></center>
+    </div>
+</body>
+</html>
